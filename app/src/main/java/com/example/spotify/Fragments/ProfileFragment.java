708e2c3d2eba4010ac3047a2f55a1e1ea0c9618e
Christina Tan <christinatan@fb.com>
2021-07-16 15:40:26 -0400

    users can now view their own posts on their timeline

--- app/src/main/java/com/example/spotify/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/spotify/Fragments/ProfileFragment.java
@@ -7,23 +7,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.spotify.Activities.LoginActivity;
+import com.example.spotify.Post;
+import com.example.spotify.PostsAdapter;
 import com.example.spotify.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
     // spotify info
     private static final String CLIENT_ID = "cae795cea2f94211bce48b701c1cfa40";
     private static final String REDIRECT_URI = "com.example.spotify://callback";
     private SpotifyAppRemote mSpotifyAppRemote;
+    public static final String TAG = "ProfileFragment";
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -33,6 +45,10 @@ public class ProfileFragment extends Fragment {
 
     private Button logoutBtn;
     private Button connectBtn;
+    private ImageView ivProfilePic;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+    private RecyclerView rvPosts;
 
     public ProfileFragment() {
         // required empty public constructor
@@ -70,6 +86,12 @@ public class ProfileFragment extends Fragment {
         // connect to xml file
         logoutBtn = view.findViewById(R.id.logoutBtn);
         connectBtn = view.findViewById(R.id.connectBtn);
+        ivProfilePic = view.findViewById(R.id.ivProfilePic);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        // initialize the array that will hold posts and create a PostsAdapter
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,6 +107,45 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 connect();
+            }
+        });
+
+        // set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        // set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        queryPosts(0);
+    }
+
+    protected void queryPosts(int i) {
+        // specify what type of data we want to query - Post.class
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // include data referred by user key
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        // limit query to latest 20 items
+        query.setLimit(20);
+        // order posts by creation date (newest first)
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+        // start an asynchronous call for posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                if (i == 1){
+                    adapter.clear();
+                }
+                // save received posts to list and notify adapter of new data, invalidates existing items and rebinds data
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
             }
         });
     }
